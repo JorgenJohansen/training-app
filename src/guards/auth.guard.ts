973a9ac45aca354bfd6c4c3b8f3b9e639ab263e0
jorgen <jorgen_buholm_johansen@hotmail.com>
2022-07-26 13:25:53 +0200

    feat: implemented and used the route guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "src/services/auth.service";
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(protected authService: AuthService, protected router: Router) {}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.authService.isAuth() ? true : this.router.navigate(['/login']);
+    }
+
+}
