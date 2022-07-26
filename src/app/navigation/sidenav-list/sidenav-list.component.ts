6d239f8845a84d61fa39149bd2c2507cb68b6ab9
jorgen <jorgen_buholm_johansen@hotmail.com>
2022-07-26 11:40:57 +0200

    added authservice and conditionally rendered links/button in sidebar

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,20 +1,32 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
+  authSub!: Subscription;
+  isAuth!: boolean;
 
-  constructor() { }
-
+  constructor(protected authService: AuthService) { }
+  
   ngOnInit(): void {
+    this.authSub = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
   }
 
   onClose(){
     this.closeSidenav.emit();
+    this.authService.logout();
   }
 
 }
