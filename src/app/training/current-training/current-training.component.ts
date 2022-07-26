e8efc32d8f1f99855baee6d20101a9b0458ad7a8
jorgen <jorgen_buholm_johansen@hotmail.com>
2022-07-26 09:30:55 +0200

    fixed some logic, passed data from dialog and start/stopped timer

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training/stop-training.component';
 
@@ -10,21 +10,37 @@ import { StopTrainingComponent } from './stop-training/stop-training.component';
 export class CurrentTrainingComponent implements OnInit {
   progress:number = 0;
   timer!: number;
+  @Output() trainingExit = new EventEmitter();
 
   constructor(private dialog:MatDialog) { }
 
   ngOnInit(): void {
+    this.startOrRestartTimer();
+  }
+
+  startOrRestartTimer(){
     this.timer = window.setInterval(() => {
       this.progress += 5;
       if(this.progress === 100){
         clearInterval(this.timer)
       }
-    },1000)
+    },1000);
   }
 
   onStop(){
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {progress: this.progress}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result){
+        this.trainingExit.emit();
+      }else{
+        this.startOrRestartTimer();
+      }
+    });
   }
+
+  
 
 }
