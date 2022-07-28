365333d6d47e99ed59efe3d113be888c0bd4a085
jorgen <jorgen_buholm_johansen@hotmail.com>
2022-07-28 14:01:42 +0200

    added functions for getting, completing and cancelling exercise, and clearing state

--- src/services/training.service.ts
+++ src/services/training.service.ts
@@ -2,7 +2,7 @@ import { Exercise } from "src/models/exercise.model";
 import { Subject } from "rxjs";
 
 export class TrainingService {
-    exerciseChanged = new Subject<Exercise>();
+    exerciseChanged = new Subject<Exercise | null>();
     private availableExercises: Exercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -10,10 +10,21 @@ export class TrainingService {
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
     ];
 
-    private runningExercise!: Exercise | undefined;
+    private runningExercise?: Exercise;
+    private completedExercises: Exercise[] = [];
+
+    constructor() {}
 
     getAvailableExercises(){
         return this.availableExercises.slice();
+    }
+    getRunningExercise(): Exercise | undefined {
+        if(this.runningExercise){
+            return {...this.runningExercise}
+        }else{
+            return undefined;
+        }
+        // return this.runningExercise ? {...this.runningExercise} : undefined;
     }
 
     startExercise(selectedId: string){
@@ -22,5 +33,30 @@ export class TrainingService {
         this.exerciseChanged.next({...this.runningExercise});
     }
 
-    constructor() {}
+    clear(){
+        delete this.runningExercise;
+        this.exerciseChanged.next(null);
+    }
+
+    completeExercise(){
+        this.completedExercises.push({
+            ...this.runningExercise as Exercise, 
+            date: new Date(),
+            state: 'completed'
+        });
+        this.clear();
+    }
+
+    cancelExercise(progress: number){
+        this.completedExercises.push({
+            ...this.runningExercise as Exercise, 
+            duration: Math.floor((this.runningExercise as Exercise).duration * (progress / 100)),
+            calories: Math.floor((this.runningExercise as Exercise).calories * (progress / 100)),
+            date: new Date(),
+            state: 'cancelled'
+        });
+        this.clear();
+    }
+
+    
 }
