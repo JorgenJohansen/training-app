36a914b1ccc66dcf4aae0b5091eecb65b2aa2e0b
jorgen <jorgen_buholm_johansen@hotmail.com>
2022-07-28 11:35:39 +0200

    added subject and made sure it can't be undefined

--- src/services/training.service.ts
+++ src/services/training.service.ts
@@ -1,6 +1,8 @@
 import { Exercise } from "src/models/exercise.model";
+import { Subject } from "rxjs";
 
 export class TrainingService {
+    exerciseChanged = new Subject<Exercise>();
     private availableExercises: Exercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -16,6 +18,8 @@ export class TrainingService {
 
     startExercise(selectedId: string){
         this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+        if (!this.runningExercise) return;
+        this.exerciseChanged.next({...this.runningExercise});
     }
 
     constructor() {}
