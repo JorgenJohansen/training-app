4d1103e8b157bc3e01ee0d920aad275955ed8e38
jorgen <jorgen_buholm_johansen@hotmail.com>
2022-07-28 16:21:35 +0200

    added ViewChild and AfterViewInit

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Exercise } from 'src/models/exercise.model';
+import { TrainingService } from 'src/services/training.service';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource<Exercise>();
+  displayedColumns = ['date','name', 'duration', 'calories', 'state'];
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() { }
+  constructor(protected trainingService:TrainingService) { }
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompletedExercises();
   }
 
 }
